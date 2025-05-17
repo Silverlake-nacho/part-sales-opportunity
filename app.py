7eba1fc32d34c15ade95b5350eb5fa6a0d28ae3f
Silverlake-nacho <166714542+Silverlake-nacho@users.noreply.github.com>
2025-05-17 11:33:37 +0100

    Update app.py

--- app.py
+++ app.py
@@ -261,13 +261,6 @@ def ebay_small_parts():
                 part_data[title]["link"] = link
             part_data[title]["count"] += 1
 
-    # After building part_data, sort it by price descending
-        sorted_parts = dict(
-        sorted(
-        part_data.items(),
-        key=lambda item: float(item[1]["price"].replace("£", "")),
-        reverse=True
-
     if not part_data:
         return "<p>No results found under £50.</p>"
 
