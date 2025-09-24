a07802130a9e24c29c2823a58017168f47440d3e
Silverlake-nacho <166714542+Silverlake-nacho@users.noreply.github.com>
2025-09-24 17:34:55 +0100

    Update app.py

--- app.py
+++ app.py
@@ -202,7 +202,7 @@ def ebay_small_parts():
     query = f"{model} {year}"
     search_url = (
         "https://www.ebay.co.uk/sch/131090/i.html?_nkw=" + query.replace(" ", "+") +
-        "&LH_ItemCondition=4&rt=nc&_sop=12&_udhi=50&LH_Complete=1&LH_Sold=1&_sop=16"
+        "&LH_ItemCondition=4&rt=nc&_sop=16&_udhi=50&LH_Complete=1&LH_Sold=1"
     )
     print("\U0001F50D eBay search URL:", search_url)
 
@@ -286,7 +286,7 @@ def ebay_medium_parts():
     query = f"{model} {year}"
     search_url = (
         "https://www.ebay.co.uk/sch/131090/i.html?_nkw=" + query.replace(" ", "+") +
-        "&LH_ItemCondition=4&rt=nc&_sop=12&_udlo=50&_udhi=500&LH_Complete=1&LH_Sold=1&_sop=15"
+        "&LH_ItemCondition=4&rt=nc&_sop=15&_udlo=50&_udhi=500&LH_Complete=1&LH_Sold=1"
         
     )
     print("\U0001F50D eBay search URL:", search_url)
@@ -371,7 +371,7 @@ def ebay_large_parts():
     query = f"{model} {year}"
     search_url = (
         "https://www.ebay.co.uk/sch/131090/i.html?_nkw=" + query.replace(" ", "+") +
-        "&LH_ItemCondition=4&rt=nc&_sop=12&_udlo=500&_udhi=5000&LH_Complete=1&LH_Sold=1&_sop=15"
+        "&LH_ItemCondition=4&rt=nc&_sop=15&_udlo=500&_udhi=5000&LH_Complete=1&LH_Sold=1"
     )
     print("\U0001F50D eBay search URL:", search_url)
 
@@ -447,6 +447,7 @@ def ebay_large_parts():
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
+
 
 
 
